26991687dadd6e1ee227c82c9d800b421f609cba
NickRest <nickrestemail@gmail.com>
2022-06-06 19:54:44 -0400

    Rotation Util

--- src/main/java/slice/util/RotationUtil.java
+++ src/main/java/slice/util/RotationUtil.java
@@ -3,21 +3,28 @@ package slice.util;
 import lombok.experimental.UtilityClass;
 import net.minecraft.client.Minecraft;
 
+/**
+ * Easier rotation methods without doing alot of math
+ *
+ * @author Nick
+ * */
 @UtilityClass
 public class RotationUtil {
 
     public static double getDirection() {
         float rotationYaw = Minecraft.getMinecraft().thePlayer.rotationYaw;
+        float moveForward = Minecraft.getMinecraft().thePlayer.moveForward;
 
-        if (Minecraft.getMinecraft().thePlayer.moveForward < 0F) rotationYaw += 180F;
+        if (moveForward < 0F)
+            rotationYaw += 180F;
 
         float forward = 1F;
 
-        if (Minecraft.getMinecraft().thePlayer.moveForward < 0F) forward = -0.5F;
-        else if (Minecraft.getMinecraft().thePlayer.moveForward > 0F) forward = 0.5F;
+        if (moveForward < 0F) forward = -0.5F;
+            else if (moveForward > 0F) forward = 0.5F;
 
-        if (Minecraft.getMinecraft().thePlayer.moveStrafing > 0F) rotationYaw -= 90F * forward;
-        if (Minecraft.getMinecraft().thePlayer.moveStrafing < 0F) rotationYaw += 90F * forward;
+        if (moveForward > 0F) rotationYaw -= 90F * forward;
+        if (moveForward < 0F) rotationYaw += 90F * forward;
 
         return Math.toRadians(rotationYaw);
     }
