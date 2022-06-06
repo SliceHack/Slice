5abd935d1026ec58359fd4a2587dcbfff62cbb81
godModeD@ <djlevy26@gmail.com>
2022-06-06 19:00:27 -0400

    add logger util

---
+++ src/main/java/slice/util/LoggerUtil.java
@@ -0,0 +1,36 @@
+package slice.util;
+
+import lombok.experimental.UtilityClass;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
+
+
+/**
+ * Utility class for logging messages to the minecraft chat.
+ *
+ * @author Dylan
+ */
+@UtilityClass
+public class LoggerUtil {
+
+    /**
+     * Logs a message to the minecraft chat.
+     *
+     * @param message The message to log.
+     */
+    public static void addMessage(String message) {
+        Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new ChatComponentText(
+                "§cSlice §7» " + message.replace("&", "§")
+        ));
+    }
+
+    /**
+     * Logs a message to the terminal.
+     *
+     * @param message The message to log.
+     */
+    public static void addTerminalMessage(String message) {
+        System.out.println("Slice » " + message.replace("&", ""));
+    }
+
+}
