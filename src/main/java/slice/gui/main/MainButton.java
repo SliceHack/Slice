e68ade5fb8d2c7beb3183769bf0e4bbde181f233
godModeD@ <djlevy26@gmail.com>
2022-06-06 23:26:41 -0400

    Nick owns me

---
+++ src/main/java/slice/gui/main/MainButton.java
@@ -0,0 +1,27 @@
+package slice.gui.main;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import slice.Slice;
+import slice.font.TTFFontRenderer;
+import slice.util.RenderUtil;
+
+public class MainButton extends GuiButton {
+
+    public MainButton(int buttonId, int x, int y, String buttonText) {
+        super(buttonId, x, y, buttonText);
+    }
+
+    public MainButton(int buttonId, int x, int y, int width, int height, String buttonText) {
+        super(buttonId, x, y, width, height, buttonText);
+    }
+
+    public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+        //draw clean button
+        int fontHeight = 50;
+        TTFFontRenderer font = Slice.INSTANCE.getFontManager().getFont("Poppins-Thin", fontHeight);
+        font.drawCenteredString(this.displayString, this.xPosition + this.width / 2f, this.yPosition + this.height / 2f - (fontHeight - 30), -1);
+        RenderUtil.drawRoundedRect(this.xPosition, this.yPosition, this.xPosition + this.width, this.yPosition + this.height, 5, -1);
+
+    }
+}
--- src/main/java/slice/gui/main/MainMenu.java
+++ src/main/java/slice/gui/main/MainMenu.java
@@ -1,13 +1,26 @@
 package slice.gui.main;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.ScaledResolution;
+import slice.Slice;
+import slice.font.TTFFontRenderer;
 
 public class MainMenu extends GuiScreen {
 
     public void initGui() {
+        ScaledResolution sr = new ScaledResolution(Minecraft.getMinecraft());
+        this.buttonList.add(new GuiButton(0, 0, 20, 150, 50,"Singleplayer"));
     }
 
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 
+        this.drawDefaultBackground();
+
+        ScaledResolution sr = new ScaledResolution(Minecraft.getMinecraft());
+        int fontHeight = 70;
+        TTFFontRenderer font = Slice.INSTANCE.getFontManager().getFont("Poppins-Thin", fontHeight);
+        font.drawStringWithShadow("Slice", sr.getScaledWidth() / 2f - (fontHeight - 30), 30, -1);
     }
 }
