09583d7e78199334162d3327e8538288d0c8d244
NickRest <nickrestemail@gmail.com>
2022-06-07 13:23:40 -0400

    NumberValue

---
+++ src/main/java/slice/setting/settings/NumberValue.java
@@ -0,0 +1,33 @@
+package slice.setting.settings;
+
+import lombok.Getter;
+import lombok.Setter;
+import slice.setting.Setting;
+
+@Getter @Setter
+public class NumberValue extends Setting {
+    private Number value, min, max;
+    private Type type;
+
+    public NumberValue(String name, Number value, Number min, Number max, Type type) {
+        super(name);
+        this.value = value;
+        this.min = min;
+        this.max = max;
+        this.type = type;
+    }
+
+    public void setValue(Number value) {
+        this.value = value;
+
+        if(value.doubleValue() < min.doubleValue()) value = min;
+        if(value.doubleValue() > max.doubleValue()) value = max;
+    }
+
+    public enum Type {
+        DOUBLE,
+        FLOAT,
+        INTEGER,
+        LONG
+    }
+}
