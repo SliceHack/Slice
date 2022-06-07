6493a3ba8832a170c5ba2b6a87341acd53985161
NickRest <nickrestemail@gmail.com>
2022-06-06 20:05:41 -0400

    Adding comments to every util (that I wrote)

--- src/main/java/slice/util/RenderUtil.java
+++ src/main/java/slice/util/RenderUtil.java
@@ -12,10 +12,22 @@ import org.lwjgl.opengl.GL11;
 @UtilityClass
 public class RenderUtil {
 
+    /**
+     * @see #drawRoundedRect(double, double, double, double, double, int)
+     */
     public static void drawRoundedRect(int x, int y, int width, int height, int color) {
         drawRoundedRect(x, y, x + width, y + width, 10, color);
     }
 
+    /**
+     * draws a rounded rectangle
+     * @parma left the left side of the rectangle
+     * @param top the top side of the rectangle
+     * @param right the right side of the rectangle
+     * @param bottom the bottom side of the rectangle
+     * @param radius the radius of the corners
+     * @param color the color of the rectangle
+     * */
     public static void drawRoundedRect(double left, double top, double right, double bottom, double radius, int color) {
         GL11.glScaled(0.5D, 0.5D, 0.5D);
         left *= 2.0D;
@@ -45,6 +57,10 @@ public class RenderUtil {
         GL11.glColor4d(1.0D, 1.0D, 1.0D, 1.0D);
     }
 
+    /**
+     * Colors using the hexadecimal color format
+     * @pamra hex the hexadecimal color
+     */
     public static void glColor(int hex) {
         float alpha = (hex >> 24 & 0xFF) / 255.0F;
         float red = (hex >> 16 & 0xFF) / 255.0F;
