b6df6182db033cffdca921bb6aff5c2aab5a8f42
NickRest <nickrestemail@gmail.com>
2022-06-07 20:59:12 -0400

    Create ClickGui

---
+++ src/main/java/slice/clickgui/ClickGui.java
@@ -0,0 +1,13 @@
+package slice.clickgui;
+
+import net.minecraft.client.gui.GuiScreen;
+
+public class ClickGui extends GuiScreen {
+
+    public void initGui() {
+    }
+
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+
+    }
+}
