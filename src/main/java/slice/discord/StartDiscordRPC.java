ca360a3af1fca885ed8f851a50ddb82416a69bb6
godModeD@ <djlevy26@gmail.com>
2022-06-09 00:49:47 -0400

    discord rpc yasss

--- src/main/java/slice/discord/StartDiscordRPC.java
+++ src/main/java/slice/discord/StartDiscordRPC.java
@@ -3,31 +3,47 @@ package slice.discord;
 import net.arikia.dev.drpc.DiscordEventHandlers;
 import net.arikia.dev.drpc.DiscordRPC;
 import net.arikia.dev.drpc.DiscordRichPresence;
+import net.arikia.dev.drpc.DiscordUser;
+import net.arikia.dev.drpc.callbacks.ReadyCallback;
+import slice.Slice;
 import slice.util.LoggerUtil;
 
 public class StartDiscordRPC {
-    long timestamp = 0;
+    private long timestamp = 0;
+    private boolean isRunning = true;
 
     public void start() {
-        timestamp = System.currentTimeMillis();
-
-        DiscordEventHandlers handlers = new DiscordEventHandlers.Builder().setReadyEventHandler((user) -> {
-            System.out.println("Welcome " + user.username + "#" + user.discriminator);
+        this.timestamp = System.currentTimeMillis();
+        DiscordEventHandlers handlers = new DiscordEventHandlers.Builder().setReadyEventHandler(new ReadyCallback() {
+            @Override
+            public void apply(DiscordUser discordUser) {
+                LoggerUtil.addTerminalMessage("Welcome " + discordUser.username + "#" + discordUser.discriminator);
+                setPresence("Slice Client", String.format("Version %s", Slice.VERSION));
+            }
         }).build();
 
-        new Thread(DiscordRPC::discordRunCallbacks).start();
-
         DiscordRPC.discordInitialize("984300399534170113", handlers, true);
+
+        new Thread(() -> {
+            while (isRunning) {
+                DiscordRPC.discordRunCallbacks();
+            }
+        }).start();
     }
 
-    public void updateDiscord(String line1, String line2) {
-        DiscordRichPresence presence = new DiscordRichPresence.Builder(line2)
-                .setBigImage("large", "")
-                .setDetails(line1)
-                .setStartTimestamps(timestamp)
-                .build();
+    public void setPresence(String line1, String line2) {
+        DiscordRichPresence.Builder presence = new DiscordRichPresence.Builder(line2);
+        presence.setBigImage("large", Slice.VERSION);
+        presence.setDetails(line1);
+        presence.setStartTimestamps(timestamp);
 
-        DiscordRPC.discordUpdatePresence(presence);
+        DiscordRPC.discordUpdatePresence(presence.build());
     }
+
+    public void shutdown() {
+        this.isRunning = false;
+        DiscordRPC.discordShutdown();
+    }
+
 
 }
