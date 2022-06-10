58681d54fa36bea991d6c69c6b75bfe11a3fadf7
godModeD@ <djlevy26@gmail.com>
2022-06-09 22:51:20 -0400

    anti bot didnt work

--- src/main/java/slice/module/modules/combat/AntiBot.java
+++ src/main/java/slice/module/modules/combat/AntiBot.java
@@ -2,11 +2,15 @@ package slice.module.modules.combat;
 
 import net.minecraft.entity.player.EntityPlayer;
 import slice.event.Event;
+import slice.event.events.EventPacket;
 import slice.event.events.EventUpdate;
 import slice.module.Module;
 import slice.module.data.Category;
 import slice.module.data.ModuleInfo;
 import net.minecraft.entity.Entity;
+import slice.util.LoggerUtil;
+import slice.util.RenderUtil;
+import slice.util.RotationUtil;
 
 @ModuleInfo(name = "AntiBot", description = "Removes all of the bots from the game.", category = Category.COMBAT)
 public class AntiBot extends Module {
@@ -14,11 +18,17 @@ public class AntiBot extends Module {
 
     public void onEvent(Event event) {
         if (event instanceof EventUpdate) {
-            for (Entity entity : mc.theWorld.loadedEntityList) {
-                if (!(entity instanceof EntityPlayer) || entity == mc.thePlayer || !entity.isInvisible()) return;
-
-                mc.theWorld.removeEntity(entity);
-            }
+//            for (Entity entity : mc.theWorld.loadedEntityList) {
+//                if (!(entity instanceof EntityPlayer)) return;
+//                if ((entity == mc.thePlayer)) return;
+//                if (!RotationUtil.isInFov(entity, 180) && !entity.onGround && entity.ticksExisted <= 5) {
+//                    mc.theWorld.removeEntity(entity);
+//                }
+//            }
         }
+
+//        if (event instanceof EventPacket) {
+//
+//        }
     }
 }
