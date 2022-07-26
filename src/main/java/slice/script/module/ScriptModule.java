b6c75deea34f3f010d52c980aa622f59c811a054
godmodeD <djlevy26@gmail.com>
2022-07-26 16:36:37 -0400

    all events

--- src/main/java/slice/script/module/ScriptModule.java
+++ src/main/java/slice/script/module/ScriptModule.java
@@ -1,6 +1,8 @@
 package slice.script.module;
 
-import slice.event.events.EventUpdate;
+import slice.event.Event;
+import slice.event.data.EventInfo;
+import slice.event.events.*;
 import slice.module.Module;
 import slice.module.data.Category;
 import slice.script.lang.Base;
@@ -44,6 +46,81 @@ public class ScriptModule extends Module {
     public void onDisable() {
         Base.callFunction(engine, "onDisable");
         super.onDisable();
+    }
+
+    @EventInfo
+    public void onUpdate(EventUpdate e) {
+        Base.callFunction(engine, "onUpdate", e);
+    }
+
+    @EventInfo
+    public void onEvent3D(Event3D e) {
+        Base.callFunction(engine, "onEvent3D", e);
+    }
+
+    @EventInfo
+    public void onEventAttack(EventAttack e) {
+        Base.callFunction(engine, "onEventAttack", e);
+    }
+
+    @EventInfo
+    public void onEventChat(EventChat e) {
+        Base.callFunction(engine, "onEventChat", e);
+    }
+
+    @EventInfo
+    public void onEventChatMessage(EventChatMessage e) {
+        Base.callFunction(engine, "onEventChatMessage", e);
+    }
+
+    @EventInfo
+    public void onEventClientBrand(EventClientBrand e) {
+        Base.callFunction(engine, "onEventClientBrand", e);
+    }
+
+    @EventInfo
+    public void onEventClientTick(EventClientTick e) {
+        Base.callFunction(engine, "onEventClientTick", e);
+    }
+
+    @EventInfo
+    public void onEventEntityRender(EventEntityRender e) {
+        Base.callFunction(engine, "onEventEntityRender", e);
+    }
+
+    @EventInfo
+    public void onEventJump(EventJump e) {
+        Base.callFunction(engine, "onEventJump", e);
+    }
+
+    @EventInfo
+    public void onEventKey(EventKey e) {
+        Base.callFunction(engine, "onEventKey", e);
+    }
+
+    @EventInfo
+    public void onEventMouse(EventMouse e) {
+        Base.callFunction(engine, "onEventMouse", e);
+    }
+
+    @EventInfo
+    public void onEventPacket(EventPacket e) {
+        Base.callFunction(engine, "onEventPacket", e);
+    }
+
+    @EventInfo
+    public void onEventPlayerReach(EventPlayerReach e) {
+        Base.callFunction(engine, "onEventPlayerReach", e);
+    }
+
+    @EventInfo
+    public void onEventSafeWalk(EventSafeWalk e) {
+        Base.callFunction(engine, "onEventSafeWalk", e);
+    }
+
+    @EventInfo
+    public void onEventSlowDown(EventSlowDown e) {
+        Base.callFunction(engine, "onEventSlowDown", e);
     }
 
 }
