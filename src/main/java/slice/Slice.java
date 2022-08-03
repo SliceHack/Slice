9874694181d95e1a1406baef663c232c382b510c
NickRest <nickrestemail@gmail.com>
2022-08-02 21:33:42 -0400

    Fix enter

--- src/main/java/slice/Slice.java
+++ src/main/java/slice/Slice.java
@@ -206,7 +206,6 @@ public enum Slice {
 
     @EventInfo
     public void onKey(EventKey e) {
-        if(e.getKey() == Keyboard.KEY_V) Minecraft.getMinecraft().displayGuiScreen(new GuiView(new Page("https://google.com")));
         if(e.getKey() == Keyboard.KEY_RSHIFT) Minecraft.getMinecraft().displayGuiScreen(clickGui);
         if (e.getKey() == Keyboard.KEY_PERIOD) Minecraft.getMinecraft().displayGuiScreen(new GuiChat("."));
         moduleManager.getModules().stream().filter(module -> module.getKey() == e.getKey()).forEach(Module::toggle); // key event
