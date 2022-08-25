0e8a7f541598e041f13297a44f724ad7af7d12a9
NickRest <nickrestemail@gmail.com>
2022-08-25 18:50:04 -0400

    BooleanValues for each disabler

--- src/main/java/slice/module/modules/misc/Disabler.java
+++ src/main/java/slice/module/modules/misc/Disabler.java
@@ -1,12 +1,8 @@
 package slice.module.modules.misc;
 
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.*;
 import net.minecraft.network.play.server.S08PacketPlayerPosLook;
-import net.minecraft.network.play.server.S32PacketConfirmTransaction;
-import slice.event.Event;
 import slice.event.data.EventInfo;
 import slice.event.data.PacketEvent;
 import slice.event.events.EventPacket;
@@ -15,12 +11,9 @@ import slice.module.Module;
 import slice.module.data.Category;
 import slice.module.data.ModuleInfo;
 import slice.setting.settings.BooleanValue;
-import slice.setting.settings.ModeValue;
-import slice.util.LoggerUtil;
 import slice.util.PacketUtil;
 
 import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
 import java.util.List;
 
 @ModuleInfo(name = "Disabler", description = "Disables an anticheat", category = Category.MISC)
