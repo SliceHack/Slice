2bec37c49ba88db9b73ec7cb9d5ed8861f271c6f
NickRest <nickrestemail@gmail.com>
2022-10-02 17:18:55 -0400

    Modes Packet & Legit for WTap

--- src/main/java/slice/module/modules/combat/WTap.java
+++ src/main/java/slice/module/modules/combat/WTap.java
@@ -9,10 +9,13 @@ import slice.event.events.EventUpdate;
 import slice.module.Module;
 import slice.module.data.Category;
 import slice.module.data.ModuleInfo;
+import slice.setting.settings.ModeValue;
 import slice.util.PacketUtil;
 
 @ModuleInfo(name = "WTap", description = "Makes people take more knock-back", category = Category.COMBAT)
 public class WTap extends Module {
+
+    ModeValue mode = new ModeValue("Mode", "Packet", "Packet", "Legit");
 
     @Getter
     private int ticks;
@@ -29,8 +32,16 @@ public class WTap extends Module {
         if(!mc.thePlayer.isSprinting()) return;
         if (ticks > 10) return;
 
-        PacketUtil.sendPacketNoEvent(new C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SPRINTING));
-        PacketUtil.sendPacketNoEvent(new C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SPRINTING));
+        switch (mode.getValue()) {
+            case "Packet":
+                PacketUtil.sendPacketNoEvent(new C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SPRINTING));
+                PacketUtil.sendPacketNoEvent(new C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SPRINTING));
+                break;
+            case "Legit":
+                if (ticks == 2) mc.thePlayer.setSprinting(false);
+                if (ticks == 3) mc.thePlayer.setSprinting(true);
+                break;
+        }
     }
 
 
