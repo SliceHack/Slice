6d63f9843f81ba6e223a9913061e7356b1b6244e
NickRest <nickrestemail@gmail.com>
2022-10-05 19:30:36 -0400

    start JarScript file

---
+++ src/main/java/com/sliceclient/script/JarScript.java
@@ -0,0 +1,13 @@
+package com.sliceclient.script;
+
+/**
+ * This file is if you want a jar instead of a javascript file for scripts.
+ *
+ * @author Nick
+ * @since 2022/10/5
+ * */
+public class JarScript {
+
+
+
+}
