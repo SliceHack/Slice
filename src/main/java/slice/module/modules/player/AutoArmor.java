4e3189f184f229e68e26e0d25d97ba54f4696364
NickRest <nickrestemail@gmail.com>
2022-12-05 11:56:31 -0500

    start autoarmor

--- src/main/java/slice/module/modules/player/AutoArmor.java
+++ src/main/java/slice/module/modules/player/AutoArmor.java
@@ -1,5 +1,10 @@
 package slice.module.modules.player;
 
 import slice.module.Module;
+import slice.module.data.Category;
+import slice.module.data.ModuleInfo;
 
-public class AutoArmor extends Module { }
+@ModuleInfo(name = "AutoArmor", description = "Automatically equips the best armor in your inventory", category = Category.PLAYER)
+public class AutoArmor extends Module {
+
+}
