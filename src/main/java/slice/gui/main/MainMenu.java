c65810a90207676b2761930309cb370357b88058
NickRest <nickrestemail@gmail.com>
2023-05-09 13:57:05 -0400

    a

--- src/main/java/slice/gui/main/MainMenu.java
+++ src/main/java/slice/gui/main/MainMenu.java
@@ -21,7 +21,7 @@ public class MainMenu extends GuiScreen {
         int y = 200;
         this.buttonList.add(new MainButton(0, x, y, "Singleplayer", () -> mc.displayGuiScreen(new GuiSelectWorld(this))));
         this.buttonList.add(new MainButton(1, x, y+60, "Multiplayer", () -> mc.displayGuiScreen(new GuiMultiplayer(this))));
-        this.buttonList.add(new MainButton(2, x, y+120, "Alt Manager", () -> mc.displayGuiScreen(new GuiAlt(this))));
+//        this.buttonList.add(new MainButton(2, x, y+120, "Alt Manager", () -> mc.displayGuiScreen(new GuiAlt(this))));
         this.buttonList.add(new MainButton(3, x, y+180, "Version Switcher", () -> mc.displayGuiScreen(new GuiProtocolSelector(this))));
         super.initGui();
     }
